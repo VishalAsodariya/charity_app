5509f7c32a932f4ec2d96ba69b37a0bff43f8dcb
VishalAsodariya <asodariya274@gmail.com>
2022-01-25 09:01:23 +0530

    First Commit

---
+++ lib/on_bording.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Onbording extends StatefulWidget {
+  const Onbording({Key? key}) : super(key: key);
+
+  @override
+  _OnbordingState createState() => _OnbordingState();
+}
+
+class _OnbordingState extends State<Onbording> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
